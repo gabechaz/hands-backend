ae67f1662a796db11e101aeb7f0bb53f91dfad26
Gabriel Chazanov <gabe.chaz@gmail.com>
2021-05-20 15:06:37 -0400

    added question routes and controller actions

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,2 +1,21 @@
 class QuestionsController < ApplicationController
+
+    def index
+        render json: Question.all
+    end
+
+    def show
+        render json: Question.find(params[:id])
+    end
+
+    def create
+        render json: Question.create(question_params)
+    end
+
+    private
+
+    def question_params
+        params.permit(:question, :answer1, :answer2)
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,9 @@ Rails.application.routes.draw do
   get '/users/:id', to: 'users#show'
   post '/users', to: 'users#create'
 
+  get '/questions', to: 'questions#index'
+  post '/questions', to: 'questions#create'
+  get '/questions/:id', to: 'questions#show'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
