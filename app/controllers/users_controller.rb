329261cb4acaf29d1af753017124c4619e9ee5bf
Gabriel Chazanov <gabe.chaz@gmail.com>
2021-05-20 14:05:01 -0400

    added user show route and controller action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,10 @@ def create
     render json: User.create(new_user_params)
 end
 
+def show
+    render json: User.find(params[:id])
+end
+
 private
 
 def new_user_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
   get '/users', to: 'users#index'
+  get '/users/:id', to: 'users#show'
   post '/users', to: 'users#create'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
