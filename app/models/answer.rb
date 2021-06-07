5f8b26af3011b2ad06092c4f89a9ffd33df79789
Gabriel Chazanov <gabe.chaz@gmail.com>
2021-06-07 14:17:37 -0400

    added question class method for finding a random question

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,9 +1,4 @@
 class Answer < ApplicationRecord
-<<<<<<< HEAD
     belongs_to :user
     belongs_to :question
-=======
-    belongs_to: :question
-    belongs_to: :user
->>>>>>> 25358b22edef200be19eab207659d34884b0185b
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,18 @@
 class Question < ApplicationRecord
     belongs_to :user
     has_many :answers
+
+
+    def random_question
+        Question.find(rand(1..Question.all.count))
+    end
+
+    def username
+        self.user.username
+    end
+
+    def answer1Frecuency
+        self.answers.select {|a| a.response == "answer1"}.count / self.answers.count
+    end
+
 end
